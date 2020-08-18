639cb0f454887c3e17fe67bc054387cfd7e57521
weizhenning <weizhenning@miduchina.com>
2020-08-18 18:04:58 +0800

    增加测试serviceImpl

---
+++ src/main/java/com/wzn/structurespring/service/imp/ProcessorServiceFourImpl.java
@@ -0,0 +1,22 @@
+package com.wzn.structurespring.service.imp;
+
+import com.wzn.structurespring.config.ProcessorConfig;
+import com.wzn.structurespring.service.BaseProcessorService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wei zhenning
+ * @version 1.0
+ * @datetime 2020-08-17 18:37
+ * @description
+ */
+@Service
+@ProcessorConfig(value = "ProcessorServiceFourImpl",status = false)
+public class ProcessorServiceFourImpl extends BaseProcessorService {
+
+    @Override
+    protected void doValidate() {
+        System.out.println("ProcessorServiceFourImpl");
+    }
+
+}
